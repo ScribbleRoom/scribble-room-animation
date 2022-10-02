57dfdfeb3afc460b79c3f56a1570340022cc08ca
Louie Richardson <60019261+LouieRichardson99@users.noreply.github.com>
2022-10-02 10:15:13 +0100

    Projects list fix

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,11 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+        <script
+          async
+          defer
+          src="https://static.cdn.prismic.io/prismic.js?new=true&repo=scribble-room-animation"
+        />
       </Head>
       <body>
         <Main />
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,8 +2,10 @@ import { SliceZone } from "@prismicio/react"
 import { createClient } from "../../prismicio"
 import { Layout } from "../../components/Layout"
 import { components } from "../../slices"
+import { ProjectCard } from "../../components/Card/Project"
+import styles from "../../sass/pages/projects-page.module.scss"
 
-const Projects = ({ data, url, lang, ...layout }) => {
+const Projects = ({ data, url, lang, projects, ...layout }) => {
   const seo = {
     metaTitle: data?.metaTitle,
     metaDescription: data?.metaDescription,
@@ -13,9 +15,26 @@ const Projects = ({ data, url, lang, ...layout }) => {
     lang,
   }
 
+  console.log(projects)
+
   return (
     <Layout seo={seo} {...layout}>
       <SliceZone slices={data?.slices} components={components} />
+      <section className={`${styles.section} container`}>
+        <ul className={styles.list}>
+          {projects.map((project, index) => (
+            <li key={index}>
+              <ProjectCard
+                link={project}
+                title={project?.data?.project_title}
+                image={project?.data?.project_image}
+                index={index}
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
+      <SliceZone slices={data?.slices2} components={components} />
     </Layout>
   )
 }
@@ -36,9 +55,14 @@ export async function getStaticProps({ previewData }) {
   const header = await client.getSingle("header")
   const footer = await client.getSingle("footer")
   const socials = await client.getSingle("socials")
+  const projects = await client.getAllByType("project", {
+    fetchLinks: ["project.project_title", "project.project_image"],
+  })
+
+  console.log(projects)
 
   return {
-    props: { header, footer, socials, ...page },
+    props: { header, footer, socials, projects, ...page },
   }
 }
 

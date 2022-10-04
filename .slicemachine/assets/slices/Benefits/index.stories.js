20b0dc16b87d6f9f532f13088784511d0f385c77
Louie Richardson <60019261+LouieRichardson99@users.noreply.github.com>
2022-10-04 10:16:29 +0100

    Benefits slice

---
+++ .slicemachine/assets/slices/Benefits/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Benefits';
+
+export default {
+  title: 'slices/Benefits'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"benefit_title":[{"type":"paragraph","text":"Aute adipisicing minim deserunt adipisicing mollit cupidatat ullamco consequat nisi. Officia in qui cupidatat minim.","spans":[]}],"benefit_description":[{"type":"paragraph","text":"Culpa est eu nostrud ad ex aliquip. Sunt qui consectetur commodo aute ad deserunt laboris aliqua dolore consequat nulla id.","spans":[]}]}],"primary":{"title":[{"type":"heading2","text":"Except","spans":[]}]},"slice_type":"benefits","id":"_Default"}} />
+_Default.storyName = ''
---
+++ slices/Benefits/index.js
@@ -0,0 +1,38 @@
+import { PrismicRichText } from "@prismicio/react"
+import styles from "./styles.module.scss"
+
+const Benefits = ({
+  slice: {
+    primary: { title },
+    items,
+  },
+}) => {
+  console.log(items)
+
+  return (
+    <section className={`container ${styles.section}`}>
+      <div className={styles.title}>
+        <PrismicRichText field={title} />
+      </div>
+      <ul className={styles.list}>
+        {items.map(({ benefit_title, benefit_description }, index) => (
+          <li key={index} className={styles.list_item}>
+            <div className={styles.index}>
+              <p>0{index + 1}.</p>
+            </div>
+            <div>
+              <div className={styles.benefit_title}>
+                <PrismicRichText field={benefit_title} />
+              </div>
+              <div className={styles.benefit_description}>
+                <PrismicRichText field={benefit_description} />
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+export default Benefits

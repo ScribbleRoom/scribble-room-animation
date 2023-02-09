8873f91883d7998e3f319d31ec23c58ec20d18c5
Louie Richardson <60019261+LouieRichardson99@users.noreply.github.com>
2023-02-09 13:30:32 +0000

    Custom form on Job application page

--- components/Form/Job/index.js
+++ components/Form/Job/index.js
@@ -1,100 +1,74 @@
-import { useState } from "react"
-import { useForm } from "react-hook-form"
-import { Input } from "../Input"
 import styles from "./styles.module.scss"
+import inputStyles from "../styles.module.scss"
 import { PaperAirplaneIcon } from "@heroicons/react/24/outline"
 
-export const JobApplicationForm = () => {
-  const [isSubmitted, setIsSubmitted] = useState(false)
-  const { register, handleSubmit } = useForm()
-
-  const encode = (data) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&")
-  }
-
-  const handleFormSubmission = async (data) => {
-    console.log(data.cv[0])
-
-    fetch(`/`, {
-      method: "POST",
-      body: encode({
-        "form-name": "job-application",
-        cv: data?.cv[0],
-        ...data,
-      }),
-    }).catch((error) => console.log(error))
-  }
-
-  const onSubmit = async (data) => {
-    await handleFormSubmission(data)
-
-    setIsSubmitted(true)
-  }
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className={styles.form}
-      name="job-application"
-      data-netlify="true"
-      id="job-application"
-      encType="multipart/form-data"
-    >
-      <input type="hidden" name="form-name" value="job-application" />
-      <div className={styles.flex_container}>
-        <Input
+export const JobApplicationForm = () => (
+  <form
+    className={styles.form}
+    name="job-application"
+    data-netlify="true"
+    id="job-application"
+    encType="multipart/form-data"
+  >
+    <input type="hidden" name="form-name" value="job-application" />
+    <div className={styles.flex_container}>
+      <div>
+        <label className={inputStyles.label} htmlFor="full_name">
+          Full Name*
+        </label>
+        <input
           name="full_name"
-          label="Full Name"
+          id="full_name"
           type="text"
-          autoComplete="name"
           required
-          register={register}
-        />
-        <Input
-          name="email"
-          label="Email"
-          type="email"
-          autoComplete="email"
-          required
-          register={register}
+          className={inputStyles.input}
         />
       </div>
-      <div className={styles.flex_container}>
-        <Input
+      <div>
+        <label className={inputStyles.label} htmlFor="email">
+          Email*
+        </label>
+        <input
+          name="email"
+          id="email"
+          type="email"
+          required
+          autoComplete="email"
+          className={inputStyles.input}
+        />
+      </div>
+    </div>
+    <div className={styles.flex_container}>
+      <div>
+        <label className={inputStyles.label} htmlFor="cv">
+          Upload CV*
+        </label>
+        <input
           name="cv"
-          label="Upload Your CV"
+          id="cv"
           type="file"
           required
-          register={register}
-        />
-        <Input
-          name="linkedin_url"
-          label="LinkedIn Profile URL"
-          type="text"
-          register={register}
+          className={inputStyles.input}
         />
       </div>
+      <div>
+        <label className={inputStyles.label} htmlFor="linkedin_url">
+          LinkedIn URL
+        </label>
+        <input
+          name="linkedin_url"
+          id="linkedin_url"
+          type="text"
+          className={inputStyles.input}
+        />
+      </div>
+    </div>
+
+    <div className={styles.button_container}>
       <button className={`button primary ${styles.button}`}>
         Submit
         <PaperAirplaneIcon />
       </button>
-      {/* {isSubmitted ? (
-        <p className={styles.submit_success_text}>
-          Thank you for submitting an job application form!
-          <br />
-          We will get back to you shortly.
-        </p>
-      ) : (
-        <div className={styles.button_container}>
-          <button className={`button primary ${styles.button}`}>
-            Submit
-            <PaperAirplaneIcon />
-          </button>
-        </div>
-      )} */}
-    </form>
-  )
-}
+    </div>
+  </form>
+)

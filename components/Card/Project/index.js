ed1d179ca13e942a4f3d5472ec4eae035c5d364e
Louie Richardson <60019261+LouieRichardson99@users.noreply.github.com>
2023-02-10 09:26:38 +0000

    Remove delay property from project card

--- components/Card/Project/index.js
+++ components/Card/Project/index.js
@@ -14,7 +14,6 @@ export const ProjectCard = ({ link, title, image, index }) => {
       opacity: 1,
       y: 0,
       transition: {
-        delay: i * 0.05,
         duration: 0.2,
       },
     }),

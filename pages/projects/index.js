5b58acbbfa8737083b688653dc43553b0658a936
Louie Richardson <60019261+LouieRichardson99@users.noreply.github.com>
2023-02-22 11:50:48 +0000

    Changed projects page layout

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,8 +2,6 @@ import { SliceZone } from "@prismicio/react"
 import { createClient } from "../../prismicio"
 import { Layout } from "../../components/Layout"
 import { components } from "../../slices"
-import { ProjectCard } from "../../components/Card/Project"
-import styles from "../../sass/pages/projects-page.module.scss"
 
 const Projects = ({ data, url, lang, projects, ...layout }) => {
   const seo = {
@@ -18,21 +16,6 @@ const Projects = ({ data, url, lang, projects, ...layout }) => {
   return (
     <Layout seo={seo} {...layout}>
       <SliceZone slices={data?.slices} components={components} />
-      <section className={`${styles.section} container`}>
-        <ul className={styles.list}>
-          {projects.map((project, index) => (
-            <li key={index}>
-              <ProjectCard
-                link={project}
-                title={project?.data?.project_title}
-                image={project?.data?.project_image}
-                index={index}
-              />
-            </li>
-          ))}
-        </ul>
-      </section>
-      <SliceZone slices={data?.slices2} components={components} />
     </Layout>
   )
 }

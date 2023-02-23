387692f4da6227de142ba0cf1ab32145abef5a64
Louie Richardson <60019261+LouieRichardson99@users.noreply.github.com>
2023-02-23 11:06:23 +0000

    Changes to hover effect

--- slices/Projects/index.js
+++ slices/Projects/index.js
@@ -7,34 +7,29 @@ const Projects = ({
     primary: { link, link_text },
     items,
   },
-}) => {
-  console.log(items)
-
-  return (
-    <section className={`${styles.section} container`}>
-      <ul className={styles.list}>
-        {items.map(({ project }, index) => (
-          <ProjectCard
-            key={index}
-            link={project}
-            title={project?.data?.project_title}
-            image={
-              project?.data.project_card_override ||
-              project?.data?.project_image
-            }
-            index={index}
-          />
-        ))}
-      </ul>
-      {link && (
-        <div className={styles.button}>
-          <PrismicLink document={link} className="button primary">
-            {link_text}
-          </PrismicLink>
-        </div>
-      )}
-    </section>
-  )
-}
+}) => (
+  <section className={`${styles.section} container`}>
+    <ul className={styles.list}>
+      {items.map(({ project }, index) => (
+        <ProjectCard
+          key={index}
+          link={project}
+          title={project?.data?.project_title}
+          image={
+            project?.data.project_card_override || project?.data?.project_image
+          }
+          index={index}
+        />
+      ))}
+    </ul>
+    {link && (
+      <div className={styles.button}>
+        <PrismicLink document={link} className="button primary">
+          {link_text}
+        </PrismicLink>
+      </div>
+    )}
+  </section>
+)
 
 export default Projects

82cae41b7785a25fed589ae4ceee19a4bdfdf54f
Louie Richardson <louie@bumpdigital.co.uk>
2024-11-14 15:16:00 +1000

    Adds text alignment option to text with image slice

--- slices/Images/LeftAlignedImageWithText/index.js
+++ slices/Images/LeftAlignedImageWithText/index.js
@@ -2,16 +2,31 @@ import { PrismicRichText } from "@prismicio/react"
 import { MotionImage } from "../MotionImage"
 import styles from "./styles.module.scss"
 
-export const LeftAlignedImageWithText = ({ image, text, text_size }) => {
+export const LeftAlignedImageWithText = ({
+  image,
+  text,
+  text_size,
+  text_alignment,
+}) => {
   const textSize = {
     Small: "text-sm",
     Medium: "text-md",
     Large: "text-lg",
   }
 
+  const textAlignment = {
+    Top: "text-top",
+    Middle: "text-middle",
+    Bottom: "text-bottom",
+  }
+
   return (
     <section className={`${styles.section} container ${textSize[text_size]}`}>
-      <div className={styles.image_text_container}>
+      <div
+        className={`${styles.image_text_container} ${
+          styles[textAlignment[text_alignment]]
+        }`}
+      >
         <MotionImage image={image} index={0} />
         <div className="flow">
           <PrismicRichText field={text} />
--- slices/Images/NarrowImageWithText/index.js
+++ slices/Images/NarrowImageWithText/index.js
@@ -1,18 +1,32 @@
 import { PrismicNextImage } from "@prismicio/next"
 import { PrismicRichText } from "@prismicio/react"
-import { MotionImage } from "../MotionImage"
 import styles from "./styles.module.scss"
 
-export const NarrowImageWithText = ({ image, text, text_size }) => {
+export const NarrowImageWithText = ({
+  image,
+  text,
+  text_size,
+  text_alignment,
+}) => {
   const textSize = {
     Small: "text-sm",
     Medium: "text-md",
     Large: "text-lg",
   }
 
+  const textAlignment = {
+    Top: "text-top",
+    Middle: "text-middle",
+    Bottom: "text-bottom",
+  }
+
   return (
     <section className={`container ${styles.section} ${textSize[text_size]}`}>
-      <div className={styles.flex_container}>
+      <div
+        className={`${styles.flex_container} ${
+          styles[textAlignment[text_alignment]]
+        }`}
+      >
         <div className={styles.image}>
           <PrismicNextImage field={image} layout="fill" />
         </div>
